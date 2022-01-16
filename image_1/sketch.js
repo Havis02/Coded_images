7c7e6da70d8ec993849ce93fc04a093d4c6920bc
Havis02 <holly2.havis@live.uwe.ac.uk>
2022-01-16 20:06:58 +0000

    set up

--- image_1/sketch.js
+++ image_1/sketch.js
@@ -1,25 +1,10 @@
 
-let x;
-let y = 0;
-let easing = 0.01;
-
 function setup() {
-        createCanvas(480,480);
-        x = width/2;
+        createCanvas(600,800);
 }
         
 function draw() {
-       if (mouseIsPressed == true){
-        stroke(255);
         background(350,65,90);
-        } else {
-        stroke(400,245,20);
-        background(760,22,280);
-        }
-let targetY = mouseY;
-       y += (targetY - y) * easing;
-       line(y,0,y,height);
-       console.log(targetY + " : " + y);
 }
   
 
