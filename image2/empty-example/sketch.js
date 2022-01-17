b6c9cff3e1de55bba9572b3a8da6ea8fb5296435
Havis02 <holly2.havis@live.uwe.ac.uk>
2022-01-17 15:50:31 +0000

    descriptions added

--- image2/empty-example/sketch.js
+++ image2/empty-example/sketch.js
@@ -1,30 +1,35 @@
 
 function setup() {
-  createCanvas(600,800);
+  createCanvas(600,800); //created canvas at fixed size//
 }
 
 function draw() {
-  background(71, 0, 179);
-  fill(255);
-  noStroke ();
-  rect(80, 120, 5, 400);
-  rect(80, 120, 400, 5);
-  rect(480, 120, 5, 400);
-  rect(80, 520, 405, 120);
-  fill(132, 132, 225);
-  ellipse(120, 170, 25, 25);
-  ellipse(140, 170, 32, 32);
-  ellipse(160, 170, 25, 25);
-  ellipse(180, 190, 25, 25);
-  ellipse(200, 190, 32, 32);
-  ellipse(220, 190, 25, 25);
-  ellipse(400, 170, 25, 25);
-  ellipse(420, 170, 32, 32);
-  ellipse(440, 170, 25, 25);
+  background(71, 0, 179); //set the background colour//
+//mountains//
+  noStroke(); //no stroke//
+  fill(170, 0, 255);
+  triangle(50, 520, 158, 220, 220, 520); //draws and postions triangle//
+  fill(255, 153, 255); 
+  triangle(80, 520, 158, 220, 280, 520);//draws and postions triangle to make 3d appearance//
+//polaroid//
+  fill(255); //sets white colour//
+  noStroke (); //no stroke//
+  rect(80, 120, 5, 400); //creates and postions rectangle on page//
+  rect(80, 120, 400, 5); //creates and postions rectangle on page//
+  rect(480, 120, 5, 400); //creates and postions rectangle on page//
+  rect(80, 520, 405, 120); //creates and postions rectangle on page//
+//clouds//
+  fill(132, 132, 225); //sets colour to blue/grey//
+  ellipse(120, 170, 25, 25); //creates and postions circle on page//
+  ellipse(140, 170, 32, 32); //creates and postions circle on page//
+  ellipse(160, 170, 25, 25); //creates and postions circle on page//
+  ellipse(180, 190, 25, 25); //creates and postions circle on page//
+  ellipse(200, 190, 32, 32); //creates and postions circle on page//
+  ellipse(220, 190, 25, 25); //creates and postions circle on page//
+  ellipse(400, 170, 25, 25); //creates and postions circle on page//
+  ellipse(420, 170, 32, 32); //creates and postions circle on page//
+  ellipse(440, 170, 25, 25); //creates and postions circle on page//
 
-  noStroke();
-  fill(255, 153, 255);
-  triangle(80, 520, 158, 220, 280, 520);
-
+  
 }
 
