1e9395633bed2c444bcbf8ac53c94a0623e8c002
Havis02 <holly2.havis@live.uwe.ac.uk>
2022-01-17 16:02:20 +0000

    finished mountains

--- image2/empty-example/sketch.js
+++ image2/empty-example/sketch.js
@@ -7,10 +7,19 @@ function draw() {
   background(71, 0, 179); //set the background colour//
 //mountains//
   noStroke(); //no stroke//
+  fill(77, 0, 56);
+  triangle(110, 520, 280, 140, 360, 520);
+  fill(153, 0, 115);
+  triangle(150, 520, 280, 140, 420, 520);
   fill(170, 0, 255);
   triangle(50, 520, 158, 220, 220, 520); //draws and postions triangle//
   fill(255, 153, 255); 
   triangle(80, 520, 158, 220, 280, 520);//draws and postions triangle to make 3d appearance//
+  fill(230, 0, 92);
+  triangle(280, 520, 460, 260, 580, 520);
+  fill(255, 51, 119);
+  triangle(300, 520, 460, 260, 620, 520);
+
 //polaroid//
   fill(255); //sets white colour//
   noStroke (); //no stroke//
