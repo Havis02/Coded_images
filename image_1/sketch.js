d1faf0c7d2fca271101c58a4471fb9dae109aa37
Havis02 <holly2.havis@live.uwe.ac.uk>
2022-01-17 11:36:21 +0000

    both triangles

--- image_1/sketch.js
+++ image_1/sketch.js
@@ -8,7 +8,8 @@ function draw() {
         noStroke (); //removes stroke//
         fill(255, 77, 196); //changes colour of the shape created below//
         ellipse(300,400,280,280); //makes a circle in a particular size and position//
-        triangle(30, 75, 58, 20, 86, 75);
+        triangle(360, 300, 300, 150, 520, 240); //makes triangle in said position and size//
+        triangle(85, 550, 320, 650, 250, 510); //makes second triangle in said position and size//
 }
   
 
