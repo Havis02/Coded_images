c0c78afa5fd5a804ee9111f25dbaeda7c949d2b0
Havis02 <holly2.havis@live.uwe.ac.uk>
2022-01-17 12:25:21 +0000

    added css

--- image_1/sketch.js
+++ image_1/sketch.js
@@ -6,8 +6,6 @@ let dots = []; // array of Jitter objects
 
 function setup() {
         createCanvas(600,800); //makes a canvas to code and 'draw' on//
-        noFill(); //will prevent fill within jittering circles//
-        stroke(255); //will make circles white//
         // Create objects
   for (let i = 0; i < 30; i++) { //creating for loop - will control how many times the shapes will jitter//
         dots.push(new Jitter(random(width), random(height), random(10, 50))); //establishes the Array being created, also makes the shapes being made random in size and jitter amount//
@@ -22,16 +20,18 @@ function mousePressed() { // create new object when mouse is pressed//
         
 function draw() {
         background(255, 204, 230); //changes the colour of the canvas//
-        noStroke (); //removes stroke//
+        noFill(); //will prevent fill within jittering circles//
+        stroke(255); //will make circles white//
         for (let i = 0; i < dots.length; i++) { // for statement to move and display all the objects//
         dots[i].move();
         dots[i].display();
       }
-        fill(255, 77, 196); //changes colour of the shape created below//
-        ellipse(300,400,280,280); //makes a circle in a particular size and position//
-        triangle(360, 300, 300, 150, 520, 240); //makes triangle in said position and size//
-        triangle(85, 550, 320, 650, 250, 510); //makes second triangle in said position and size//
-        
+      noStroke (); //removes stroke//
+      fill(255, 77, 196); //changes colour of the shape created below//
+      ellipse(300,400,280,280); //makes a circle in a particular size and position//
+      triangle(360, 300, 300, 150, 520, 240); //makes triangle in said position and size//
+      triangle(85, 550, 320, 650, 250, 510); //makes second triangle in said position and size//
+ 
 }
 
 //Jitter class//
