a8b3cac9dccb0a57a99963249aebfb56bb6472e3
Havis02 <holly2.havis@live.uwe.ac.uk>
2022-01-17 12:33:12 +0000

    rearranged shapes

--- image_1/sketch.js
+++ image_1/sketch.js
@@ -29,9 +29,9 @@ function draw() {
       }
       stroke (204, 0, 136); //removes stroke//
       fill(255, 77, 196); //changes colour of the shape created below//
-      ellipse(300,400,280,280); //makes a circle in a particular size and position//
       triangle(360, 300, 300, 150, 520, 240); //makes triangle in said position and size//
       triangle(85, 550, 320, 650, 250, 510); //makes second triangle in said position and size//
+      ellipse(300,400,280,280); //makes a circle in a particular size and position//
       
  
 }
