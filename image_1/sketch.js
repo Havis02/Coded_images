892ef0909522a0ccb87e02edd3ea779f3bbff657
Havis02 <holly2.havis@live.uwe.ac.uk>
2022-01-17 12:32:10 +0000

    added stroke

--- image_1/sketch.js
+++ image_1/sketch.js
@@ -1,3 +1,4 @@
+
 let x; //sets x value//
 let y; //sets y value//
 let diameter; //sets diameter//
@@ -26,11 +27,12 @@ function draw() {
         dots[i].move();
         dots[i].display();
       }
-      noStroke (); //removes stroke//
+      stroke (204, 0, 136); //removes stroke//
       fill(255, 77, 196); //changes colour of the shape created below//
       ellipse(300,400,280,280); //makes a circle in a particular size and position//
       triangle(360, 300, 300, 150, 520, 240); //makes triangle in said position and size//
       triangle(85, 550, 320, 650, 250, 510); //makes second triangle in said position and size//
+      
  
 }
 
