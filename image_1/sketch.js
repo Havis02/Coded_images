5558940f2cc09af8f2c5bcc42fc3e13a3d9c7828
Havis02 <holly2.havis@live.uwe.ac.uk>
2022-01-17 10:59:27 +0000

    circle created

--- image_1/sketch.js
+++ image_1/sketch.js
@@ -1,12 +1,14 @@
 
 function setup() {
-        createCanvas(600,800); //
+        createCanvas(600,800); //makes a canvas to code and 'draw' on//
 }
         
 function draw() {
-        background(350,65,90);
-        noStroke ();
-        ellipse(300,400,280,280);
+        background(255, 204, 230); //changes the colour of the canvas//
+        noStroke (); //removes stroke//
+        fill(255, 77, 196); //changes colour of the shape created below//
+        ellipse(300,400,280,280); //makes a circle in a particular size and position//
+        
 }
   
 
