ddfa90ad0acdd9961f17e5e8529b8e7fd14907f8
Havis02 <holly2.havis@live.uwe.ac.uk>
2022-01-17 11:55:36 +0000

    creating array

--- image_1/sketch.js
+++ image_1/sketch.js
@@ -1,6 +1,17 @@
+let x; //sets x value//
+let y; //sets y value//
+let diameter; //sets diameter//
+let speed; //sets speed//
+let dots = []; // array of Jitter objects
 
 function setup() {
         createCanvas(600,800); //makes a canvas to code and 'draw' on//
+        noFill(); //will prevent fill within jittering circles//
+        stroke(255); //will make circles white//
+        // Create objects
+  for (let i = 0; i < 50; i++) { //creating for loop - will control how many times the shapes will jitter//
+        dots.push(new Jitter(random(width), random(height), random(10, 30))); //establishes the Array being created, also makes the shapes being made random in size and jitter amount//
+      }
 }
         
 function draw() {
@@ -10,6 +21,7 @@ function draw() {
         ellipse(300,400,280,280); //makes a circle in a particular size and position//
         triangle(360, 300, 300, 150, 520, 240); //makes triangle in said position and size//
         triangle(85, 550, 320, 650, 250, 510); //makes second triangle in said position and size//
+        
 }
   
 
