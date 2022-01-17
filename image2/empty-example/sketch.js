6c235556bedaad7ac1ff202c4a44eb97f4df1c1a
Havis02 <holly2.havis@live.uwe.ac.uk>
2022-01-17 14:11:17 +0000

    needed new files

--- image2/empty-example/sketch.js
+++ image2/empty-example/sketch.js
@@ -1,24 +1,7 @@
-let myArr = [];
-xPos = 0;
-
 function setup() {
-  createCanvas(1024,400)
-  for (let i = 0; i < 256; i++) {
-          myArr.push(i)
-          console.log(myArr)
-  }
-      
-
-
+  // put setup code here
 }
 
 function draw() {
-  background(255)
-  noStroke();
-  noLoop();
-  for (let i=0; i < myArr.length; i++){
-    fill(myArr[i],70,230);
-    rect(xPos,0,width/256,height);
-    xPos = xPos+(width/256);
- }
+  // put drawing code here
 }
