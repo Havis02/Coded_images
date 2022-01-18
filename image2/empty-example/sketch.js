03428986b0edbdb484dc403660d2ed7c7e757a6c
Havis02 <holly2.havis@live.uwe.ac.uk>
2022-01-18 13:04:10 +0000

    lots of added code and descriptions

--- image2/empty-example/sketch.js
+++ image2/empty-example/sketch.js
@@ -1,10 +1,20 @@
 
 function setup() {
   createCanvas(600,800); //created canvas at fixed size//
+  
 }
 
 function draw() {
-  background(71, 0, 179); //set the background colour//
+  //background(71, 0, 179); //set the background colour//
+  c1 = color(41, 0, 102); //picks colour for gradient//
+  c2 = color(117, 26, 255); //picks colour for gradient//
+  for(let y=0; y<height; y++){ //for loop, declaring the height 'length' of the gradient//
+    n = map(y,0,height,0,1); //'maps' out when the gradient will start and end//
+    let newc = lerpColor(c1,c2,n); //let valuable to link and blend the two chosen colours together//
+    stroke(newc); //calls the let variable//
+    line(0,y,width, y); //stops the gradient from looking 'blocky'//
+  }
+
 //mountains//
   noStroke(); //no stroke//
   fill(77, 0, 56);
@@ -39,6 +49,28 @@ function draw() {
   ellipse(420, 170, 32, 32); //creates and postions circle on page//
   ellipse(440, 170, 25, 25); //creates and postions circle on page//
 
+  fill(89, 0, 179);
+  triangle(485, 520, 485, 260, 620, 520);
+  fill(78, 0, 170);
+  triangle(485, 350, 485, 260, 550, 350);
+  fill(102, 0, 204);
+  triangle(50, 520, 80, 430, 80, 520);
+
+
   
+  noStroke();
+  fill(230, 0, 172);
+  ellipse(600, 760, 550, 550);
+  fill(255, 51, 204);
+  ellipse(630, 790, 550, 550);
+  fill(255, 102, 217);
+  ellipse(660, 820, 550, 550);
+  fill(255, 128, 255);
+  ellipse(690, 850, 550, 550);
+  fill(117, 26, 255);
+  ellipse(720, 880, 550, 550);
+
 }
+
+
 
