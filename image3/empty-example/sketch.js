fb31f13d09334a6ded39a7dc228d359b06b25638
Havis02 <holly2.havis@live.uwe.ac.uk>
2022-01-18 17:06:35 +0000

    added constellation

--- image3/empty-example/sketch.js
+++ image3/empty-example/sketch.js
@@ -1,5 +1,11 @@
+// an array to add multiple particles
+let particles = [];
+
 function setup() {
   createCanvas(800,600); //creating canvas//
+  for(let i = 0;i<width/10;i++){
+    particles.push(new Particle());
+  }
 }
 
 function draw() {
@@ -10,6 +16,10 @@ function draw() {
     let newc = lerpColor(c1,c2,n); //let valuable to link and blend the two chosen colours together//
     stroke(newc); //calls the let variable//
     line(0,y,width, y); //stops the gradient from looking 'blocky'//
+  }
+  for(let i = 0;i<particles.length;i++) {
+    particles[i].createParticle();
+    particles[i].joinParticles(particles.slice(i));
   }
   noStroke();
   fill(217, 179, 255);
@@ -45,4 +55,54 @@ function draw() {
   fill(230, 0, 153);
   ellipse(50, 365, 40, 15);
 
+  drawshape_bubbles();
+}
+
+function drawshape_bubbles(){
+  fill(102, 194, 255);
+  ellipse(230, 365, 45, 45);
+  ellipse(380, 315, 35, 35);
+  fill(10, 153, 255);
+  ellipse(430, 300, 45, 45);
+  ellipse(730, 420, 35, 35);
+  fill(187, 153, 255);
+  ellipse(200, 315, 25, 25);
+  ellipse(700, 375, 25, 25);
+  fill(255);
+  ellipse(245, 360, 8, 15);
+  ellipse(390, 310, 5, 10);
+  ellipse(445, 295, 7, 16);
+  ellipse(740, 420, 5, 12);
+  ellipse(208, 315, 5, 7);
+  ellipse(708, 375, 5, 7);
+
+}
+// this class describes the properties of a single particle.
+class Particle {
+  // setting the co-ordinates, radius and the
+  // speed of a particle in both the co-ordinates axes.
+    constructor(){
+      this.x = random(2,width);
+      this.y = random(2,height);
+      this.r = random(3,10);
+    
+    }
+    // creation of a particle.
+  createParticle() {
+    noStroke();
+    fill(255, 255, 179);
+    circle(this.x,this.y,this.r);
+  }
+  // this function creates the connections(lines)
+// between particles which are less than a certain distance apart
+joinParticles(particles) {
+  particles.forEach(element =>{
+    let dis = dist(this.x,this.y,element.x,element.y);
+    if(dis<85) {
+      stroke(255, 255, 230);
+      strokeWeight(1.5);
+      line(this.x,this.y,element.x,element.y);
+    }
+  });
+}
 }
